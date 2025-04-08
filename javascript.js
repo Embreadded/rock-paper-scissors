83f57bf07d717f694f4644dac84df45b4b13ed5a
Embreadded <167567084+Embreadded@users.noreply.github.com>
2025-04-08 16:01:53 -0400

    Created getComupterChoice for the computer to randomize through "Rock Paper and Scissors".
    As well as begun code for human input and
    win conditions

--- javascript.js
+++ javascript.js
@@ -1,1 +1,27 @@
-console.log("Hello World")
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "Rock";
+    } else if (randomNumber === 1) {
+        return "Paper";
+    } 
+        return "Scissors";
+    }
+
+
+//create function to return "rock" "paper" "scissors"//
+
+//create prompt for user input and limit choices to only allow "rock" "paper" "scissors"//
+
+function getHumanChoice() {
+    if (getHumanChoice === "Rock" && getComputerChoice === "Scissors") {
+        return "You Win!";
+    } else if (getHumanChoice === "Paper" && getComputerChoice === "Rock") {
+        return "You Win!";
+    } else if (getHumanChoice === "Scissors" && getComputerChoice === "Paper") {
+        return "You Win!";
+    } return "it's a Draw"
+
+}
+
+console.log();
