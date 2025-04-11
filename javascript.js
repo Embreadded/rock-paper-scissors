8bea5b403466a7e26a98168f776ca2a9c385144d
Embreadded <167567084+Embreadded@users.noreply.github.com>
2025-04-11 00:19:54 -0400

    Added text to wedpage to explain how to restart
    Had to rewrite and reorder code to make it easier to read

--- javascript.js
+++ javascript.js
@@ -1,57 +1,58 @@
-function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice === 0) {
-        return "rock";
-    } else if (computerChoice === 1) {
-        return "paper";
-    } 
-        return "scissors";
-    }
-
 function getHumanChoice() {
-    let humanChoice = prompt("Will you pick Rock, Paper or Scissors?");
-    console.log(humanChoice);
-    return humanChoice;
+    const humanChoice = prompt("What will you pick? Rock, Paper or Scissors?").toLowerCase();
+    if (
+        humanChoice === "rock" ||
+        humanChoice === "paper" ||
+        humanChoice === "scissors"
+    ) {
+        return humanChoice;
+    } else {
+        alert("Please enter a valid option")
+    }
 }
 
-var humanScore = 0;
-var computerScore = 0;
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+   
+    if (computerChoice === 0) {
+       return "rock";
+    } else if (computerChoice === 1){
+       return "paper";
+    } else {
+       return "scissors";
+    }
+   }
+
+let humanScore = 0;
+let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
-    if (getHumanChoice === "rock" && getComputerChoice === "scissors") {
-        console.log("Rock beats Scissors! You Win!");
-        humanScore += 1
-        return;
-    } 
-    else if (getHumanChoice == "paper" && getComputerChoice == "rock") {
-        console.log("Paper beats Rock! You Win!");
-        humanScore += 1
-        return;
-    } 
-    else if (getHumanChoice == "scissors" && getComputerChoice == "paper") {
-        console.log("Scissors beats Paper! You Win!");
-        humanScore += 1
-        return;
-    } 
-    else if (getHumanChoice == "paper" && getComputerChoice == "scissors") {
-        console.log("Scissors beats Paper! You Lose!");
-        computerScore += 1
-        return;
-    }
-    else if (getHumanChoice == "paper" && getComputerChoice == "rock") {
-        console.log("Paper beats Rock! You Lose!");
-        computerScore += 1
-        return;
-    }
-    else if (getHumanChoice == "rock" && getComputerChoice == "scissors") {
-        console.log("Rock beats Scissors! You Lose!");
-        computerScore += 1
-        return;
-    } 
-    else {
-        console.log ("It's a Draw!")
-        return;
-    }
-}
 
+    if (humanChoice === computerChoice) {
+        return "It's a tie!"
+    } 
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ) {
+            alert("You Win! " + humanChoice + " beats " + computerChoice);
+            humanScore ++;
+            console.log(humanScore);
+        }
+        else if (
+            (humanChoice === "paper" && computerChoice === "rock") ) {
+                alert("You Win! " + humanChoice + " beats " + computerChoice);
+                humanScore ++;
+                console.log(humanScore);
+        }   
+        else if (
+        (humanChoice === "scissors" && computerChoice === "paper") ) {
+            alert("You Win! " + humanChoice + " beats " + computerChoice);
+            humanScore ++;
+            console.log(humanScore);
+        }
+        else {
+            alert("You Lose! " + computerChoice +" beats " + humanChoice);
+            computerScore ++;
+            console.log(computerScore);
+        }
+    }
